fead0ef8c72a4f695f4dcaf4099ddce2e4ee22e6
Mustaf Khire <mustafa.khire@gmail.com>
2024-05-02 12:40:28 -0400

    Final CRUD Project

--- website/views.py
+++ website/views.py
@@ -5,6 +5,7 @@ from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
+
 def home(request):
     records = Record.objects.all()
     # Check to see if logged in
@@ -75,6 +76,19 @@ def add_record(request):
                 messages.success(request, "Record Added..")
                 return redirect('home')
         return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in!")
+        return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance= current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
     else:
         messages.success(request, "You must be logged in!")
         return redirect('home')
