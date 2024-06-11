490e090805b7fb59c05297ee9eabe6d8c40c31ed
Mustaf Khire <mustafa.khire@gmail.com>
2024-06-11 16:31:14 -0400

    final commit

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 #Build dcrm project
 echo " Building the project..."
-python3.8 -m pip install -r requirements.txt
+python3.8.0 -m pip install -r requirements.txt
 
 echo " Make Migration..."
-python3.8 manage.py makemigrations --noinput
-python3.8 manage.py migrate --noinput
+python3.8.0 manage.py makemigrations --noinput
+python3.8.0 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.8 manage.py collectstatic --noinput --clear
+python3.8.0 manage.py collectstatic --noinput --clear
